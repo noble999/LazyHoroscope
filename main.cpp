bd9431850c01a7b81aab91c4650dfb0a743dd55f
noble999 <nleduc2@mail.csuchico.edu>
2019-09-23 16:21:57 -0700

    new lazyhoroscope

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
   string first_Name=" ";
   string last_Name=" ";
+  int lucky=0;
   //get user input
   cout<<"What is your first name?\n";
   cin>>first_Name;
@@ -16,5 +17,7 @@ int main()
   cin>>last_Name;
   //tell fortune
   cout<<"Welcome, "<<first_Name[0]<<"."<<last_Name[0]<<"., here is your fortune...";
+  lucky=first_Name.length();
+  cout<<"\nyour lucky number is "<<lucky;
   return 0;
 }
