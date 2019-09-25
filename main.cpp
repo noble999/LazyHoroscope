10284f94d81c3537113cf47bac0f91fe498295f6
noble999 <nleduc2@mail.csuchico.edu>
2019-09-25 16:46:25 -0700

    lazy horoscopes part B

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-//Authors:
+//Authors:Noble LeDuc
 #include<iostream>
 #include<string>
+#include<ctype.h>
+#include<stdio.h>
 
 using namespace std;
 
@@ -13,11 +15,34 @@ int main()
   //get user input
   cout<<"What is your first name?\n";
   cin>>first_Name;
+  first_Name[0]=toupper(first_Name[0]);
   cout<<"What is your last name?\n";
   cin>>last_Name;
+  last_Name[last_Name.length()-1]=toupper(last_Name[last_Name.length()-1]);
   //tell fortune
   cout<<"Welcome, "<<first_Name[0]<<"."<<last_Name[0]<<"., here is your fortune...";
   lucky=first_Name.length();
-  cout<<"\nyour lucky number is "<<lucky<<endl;
+  cout<<"\nyour lucky number is "<<lucky;
+  if((first_Name[0]=='A')||(first_Name[0]=='E')||(first_Name[0]=='I')||(first_Name[0]=='O')||(first_Name[0]=='U'))
+  {
+   cout<<"\nyou are destined to be famous!";
+  }
+  else
+  {
+   cout<<"\nyou should keep a low profile.";
+  }
+  if((last_Name[last_Name.length()-1]=='A')||(last_Name[last_Name.length()-1]=='E')||
+     (last_Name[last_Name.length()-1]=='I')||(last_Name[last_Name.length()-1]=='O')||
+     (last_Name[last_Name.length()-1]=='U'))
+  {
+	cout<<"\nyou have already met your true love.";
+        cout<<"\nhave a good day!";
+  }
+  else
+  {
+	cout<<"\nhave a good day!";
+  }
+	cout<<endl;
+
   return 0;
 }
